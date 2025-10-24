12baf34011eb628bd496c2f12ea7de9220b8053f
sulakshanadeore <deoresulakshana@gmail.com>
2025-10-24 20:14:05 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using FirstWebAppMVC_Star.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAppMVC_Star.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAppMVC_Star.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username)
+        {
+            string time=ShowTime();
+
+            return RedirectToAction("Greet", new { name = username,loggedintime=time });
+        }
+
+        [NonAction]
+        public string ShowTime()
+        { 
+            return DateTime.Now.ToLongTimeString();
+        }
+
+
+        public ContentResult Greet(string name,string loggedintime)
+        {
+
+            return Content("Welcome to MVC " + name + "at " + loggedintime);
+        }
+
+        public ActionResult OpenGoogle()
+        {
+
+            return RedirectPermanent("https://www.google.com");
+        }
+
+        public ActionResult GoToHome()
+        {
+            return RedirectToAction("Greet");
+        }
+
+        public ActionResult OpenIndexOfHome()
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
